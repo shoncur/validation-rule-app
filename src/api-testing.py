71262de4c21bb70a20571da6f407f9cc26fa676a
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2023-12-04 11:43:52 -0700

    Can find type of change

--- src/api-testing.py
+++ src/api-testing.py
@@ -29,14 +29,33 @@ while login_fail:
 valid_co = False
 while not valid_co:
     try:
+        # Searching through Arena to find the specific CO that the user has inputted
         print('Enter the 6 digits of the CO you would like to test')
         co = 'CO-' + input('CO-')
         co_url = f'{BASE_URL}/changes?number={co}'
         co_headers = {'arena_session_id':f'{arena_session_id}', 'Content-Type':'application/json'}
         co_response = requests.get(co_url, headers=co_headers).json()
+        # print(co_response, '\n')
+        co_guid = co_response['results'][0]['guid']
 
-        print(co_response)
+        # To get more information, we are going to grab the guid that was found and query for it
+        co_url = f'{BASE_URL}/changes/{co_guid}'
+        co_response = requests.get(co_url, headers=co_headers).json()
+
+        # Find the 'Type of Change' attribute
+        type_of_change_attribute = next(
+            (attr for attr in co_response.get('additionalAttributes', []) if attr.get('name') == 'Type of Change'),
+            None
+        )
+
+        # Extract the value if the attribute is found
+        type_of_change_value = type_of_change_attribute.get('value') if type_of_change_attribute else None
+
+        print("Type of Change:", type_of_change_value)
+
+        valid_co = True
     except HTTPError as http_error:
         print(f'HTTP error occurred: {http_error}')
     except Exception as error:
         print(f'Invalid entry: {error}')
+
