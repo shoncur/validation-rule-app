d9c647dffccdaef0044d328961214a964eafed0a
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2023-12-04 14:32:44 -0700

    grabs all files in doc update

--- src/api-testing.py
+++ src/api-testing.py
@@ -9,18 +9,29 @@ url = f'{BASE_URL}/login'
 headers = {'Content-Type':'application/json'}
 
 def process_initial_release(change_data):
-    # Logic for handling Initial Release
-    pass
+    # Check that Item Tab has Files attached
+    # Under 'Items Tab Modifications' the 'Item New Phase' should only have the new revision
+    # Under 'Items Tab Modifications' the Specs, BOM Source Cost and Files should be checked
+    print('--Initial Release has been called--')
 
 def process_document_update(change_data):
-    # Logic for handling Document Updates
-    pass
+    # Check that there are redlines included in the files
+    print('--Document Update has been called--')
+    file_url = f'{BASE_URL}/changes/{co_guid}/files'
+    file_response = requests.get(file_url, headers=co_headers).json()
+    print(file_response)
+
+def process_lifecycle_update(change_data):
+    # Check that lifecycle has been updated
+    print('--Lifecycle Update has been called--')
 
 def dispatch_process(type_of_change_value, change_data):
     if type_of_change_value == "Initial Release":
-        process_document_update(change_data)
+        process_initial_release(change_data)
     elif type_of_change_value == "Document Update":
         process_document_update(change_data)
+    elif type_of_change_value == "Lifecycle Update":
+        process_lifecycle_update(change_data)
     # Add more conditions as needed
     else:
         print("Unsupported type of change:", type_of_change_value)
@@ -39,8 +50,8 @@ password = config.get('password')
 login_fail = True
 while login_fail:
     try:
-        email = input('Enter email: ')
-        password = getpass.getpass('Enter password: ')
+        # email = input('Enter email: ')
+        # password = getpass.getpass('Enter password: ')
         data = {
             'email':f'{email}',
             'password':f'{password}'
@@ -81,11 +92,10 @@ while not valid_co:
         # Extract the value if the attribute is found
         type_of_change_value = type_of_change_attribute.get('value') if type_of_change_attribute else None
 
-        print("Type of Change:", type_of_change_value)
-
         # Perform the dispatch
         if type_of_change_value:
-            dispatch_process(type_of_change_value[0], data)
+            for type in type_of_change_value:
+                dispatch_process(type, data)
         else:
             print("Type of Change not found.")
 
